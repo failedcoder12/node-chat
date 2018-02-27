b8209f606a4362b37d388a7c1b97b578c59c5b82
failedcoder12 <divyanshverma12@gmail.com>
2018-02-28 00:10:29 +0530

    form GUI

--- public/js/index.js
+++ public/js/index.js
@@ -13,9 +13,25 @@ socket.on('disconnect',()=>{
 
 socket.on('newMessage',function(message) {
 	console.log('newMessage',message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage',{
-	from: 'Pokemon',
-	text: 'hi'
+// socket.emit('createMessage',{
+// 	from: 'Pokemon',
+// 	text: 'hi'
+// },function (value) {
+// 	console.log(value,'Oh a pokemon');
+// });
+
+jQuery('#message-form').on('submit',function(e){
+	e.preventDefault();
+	socket.emit('createMessage',{
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	},function(){
+
+	});
 });
