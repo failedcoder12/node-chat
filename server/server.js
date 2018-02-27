354be3aca04c8fdbc7784c7137809b00165d8593
16CO110 <16co110@example.com>
2018-02-27 21:32:50 +0530

    second

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 // console.log(__dirname + '/../public');
@@ -7,9 +9,12 @@ const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+server.listen(port, ()=> {
 	console.log(`Server is up on ${port}`);
 });
+
+console.log('hello world');
