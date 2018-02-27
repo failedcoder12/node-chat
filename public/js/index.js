9707da12a8a5a921f9687375f734746ec1dac3c4
16CO110 <16co110@example.com>
2018-02-27 22:19:43 +0530

    mesage sending

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+socket.on('connect',()=>{
+		console.log('Connected to server');
+});
+socket.on('disconnect',()=>{
+		console.log('Disconnected from server');
+});
+
+socket.emit('createMessage',{
+	from: 'Admin',
+	text: 'Yep. That works for me'
+});
+
+socket.on('newMessage',function(message) {
+	console.log('newMessage',message);
+});
--- server/server.js
+++ server/server.js
@@ -10,11 +10,28 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+	console.log('New user connected');
+
+socket.on('createMessage',(message)=> {
+	console.log('createMessage',message);
+});
+
+socket.emit('newMessage',{
+	from: 'User',
+	text: 'Ok',
+	createAt: 123
+});
+	socket.on('disconnect',()=>{
+		console.log('User was disconnected');
+	});
+});
 
 server.listen(port, ()=> {
 	console.log(`Server is up on ${port}`);
 });
 
-console.log('hello world');
